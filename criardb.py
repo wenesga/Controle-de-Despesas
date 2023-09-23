2f3477edd21f002563d8033b2ab5a684c0478f84
wenesga@gmail.com <wenesga@gmail.com>
2023-09-23 06:57:31 -0300

    Início do desenvolvimento de controle de despesas

---
+++ criardb.py
@@ -0,0 +1,9 @@
+import sqlite3 as lite  # Importando SQLite
+
+""" Arraias - TO - 2023 - UFT - Projeto Pessoal
+    Contrele de Despesas
+    @Autor: Wenes Aquino               """
+# -------------------------------------------------------------------------S-----
+
+# Criando coneccao
+com = lite.connect('dados.db')
