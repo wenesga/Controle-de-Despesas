a8772beffaaed3e02e015861c2a68e6b4d4c9d34
wenesga@gmail.com <wenesga@gmail.com>
2023-09-23 07:57:22 -0300

    Início do desenvolvimento de controle de despesas

--- criardb.py
+++ criardb.py
@@ -1,9 +1,26 @@
 import sqlite3 as lite  # Importando SQLite
 
-""" Arraias - TO - 2023 - UFT - Projeto Pessoal
-    Contrele de Despesas
-    @Autor: Wenes Aquino               """
+""" Projeto: Controle de Despesas Pessoal
+    @Autor: Wenes Aquino              """
 # -------------------------------------------------------------------------S-----
 
 # Criando coneccao
 com = lite.connect('dados.db')
+
+# Criando tebela de categoria
+with com:
+    cur = com.cursor()
+    cur.execute(
+        "CREATE TABLE Categoria(id INTEGER PRIMATY KEY, nome TEXT)")
+
+# Criando tebela de receita
+with com:
+    cur = com.cursor()
+    cur.execute(
+        "CREATE TABLE Receitas(id INTEGER PRIMATY KEY, categoria TEXT, adicionado_em DATE, valor DECIMAL)")
+
+# Criando tebela de gasto
+with com:
+    cur = com.cursor()
+    cur.execute(
+        "CREATE TABLE Gastos(id INTEGER PRIMATY KEY, categoria TEXT, retirado_em DATE, valor DECIMAL)")
